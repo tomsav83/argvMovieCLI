dfbb2d82895007fc4a1a5f65020a8c968291be51
Tomsav83 <tomsav83@gmail.com>
2021-12-14 12:27:51 +0000

    With storage.json

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 const yargs = require('yargs');
-const Movie = require('./utils/utils.js');
+const { Movie, pullMovieData } = require('./utils/utils.js');
 const args = yargs.argv;
 
 const app = () => {
+  pullMovieData();
   switch (args.command) {
     case 'add':
-      // function that adds movie from process.argv into an array and console.logs the array.
+      // function that adds movie from process.argv into an array
       const newMovie = new Movie(args.title, args.actor, args.additionalInfo);
 
       newMovie.add();
       newMovie.list();
+      newMovie.save();
+
       break;
 
     case 'add multi':
@@ -17,6 +20,8 @@ const app = () => {
       const movie2 = new Movie(args.title2, args.actor2, args.additionalInfo2);
       movie1.add();
       movie2.add();
+      movie1.save();
+      movie2.save();
       movie1.list();
       break;
 
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,14 @@
-const movieArr = [];
+const fs = require('fs');
+let movieArr;
+// JSON.parse will convert from JSON to a JS object so speechmarks around keys and integers will be gone
+// readFileSync will grab information from file (storage.json) if it exists
+const pullMovieData = () => {
+  try {
+    movieArr = JSON.parse(fs.readFileSync('./storage.json'));
+  } catch (error) {
+    movieArr = [];
+  }
+};
 
 class Movie {
   constructor(
@@ -13,11 +23,16 @@ class Movie {
 
   add() {
     movieArr.push(this);
-    console.log(movieArr);
   }
   list() {
     console.log(movieArr);
   }
+  save() {
+    // save movie array to json file
+    fs.writeFileSync('./storage.json', JSON.stringify(movieArr));
+    // writeFileSync will find file, if it doesn't exist it will create it
+    // movieArr needs to be convested into JSON by using JSON.stringify method
+  }
 }
 
-module.exports = Movie;
+module.exports = { Movie, pullMovieData };
